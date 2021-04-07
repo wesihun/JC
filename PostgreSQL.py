540cbaba40ce700b59915e234887f9de9aee323b
Administrator <1498731041@qq.com>
2021-04-07 16:58:39 +0800

    Default Changelist

--- PostgreSQL.py
+++ PostgreSQL.py
@@ -4,11 +4,11 @@ from flask import current_app
 
 
 class PostgreSQL(object):
-    def __init__(self, **args):
-        self.args = args
+    def __init__(self):
+        pass
 
     def getConnection(self):
-        connection = psycopg2.connect(database=self.args['DATABASE'], user=self.args['USER'], password=self.args['PASSWORD'], host=self.args['HOST'], port=self.args['PORT'])
+        connection = psycopg2.connect(database=current_app.config['DATABASE'], user=current_app.config['USER'], password=current_app.config['PASSWORD'], host=current_app.config['HOST'], port=current_app.config['PORT'])
         return connection
 
     def closeConnection(self, connection):
@@ -21,13 +21,35 @@ class PostgreSQLService(object):
 
     def get_user_by_name_pass(self, username, password):
         """查询用户根据用户名和密码"""
-        postgreSQL = PostgreSQL(DATABASE=current_app.config['DATABASE'], USER=current_app.config['USER'], PASSWORD=current_app.config['PASSWORD'], HOST=current_app.config['HOST'], PORT=current_app.config['PORT'])
+        postgreSQL = PostgreSQL()
 
         try:
             connection = postgreSQL.getConnection()
             cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)  # 字典
 
-            cursor.execute("select id, username, password, to_char(createtime, 'yyyy-MM-dd hh24:mm:ss') as createtime from tb_user where username='%s' and password='%s'" % (username, password))
+            cursor.execute('''select id, username, password, to_char(createtime, 'yyyy-MM-dd hh24:mm:ss') as createtime 
+                              from tb_user 
+                              where username='%s' and password='%s' ''' % (username, password))
+            data = cursor.fetchall()
+        except Exception as error:
+            print(error.with_traceback())
+        finally:
+            postgreSQL.closeConnection(connection)
+
+        return data
+
+    def get_device_by_page(self, pageSize=1, currentPage=1):
+        """分页取得设备"""
+        postgreSQL = PostgreSQL()
+
+        try:
+            connection = postgreSQL.getConnection()
+            cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)  # 字典
+
+            cursor.execute('''select tb_device_type.name as type,tb_device.id,tb_device.number,tb_device.name,tb_device.project,tb_device.person,tb_device.state,tb_device.locate
+                              from tb_device_type,tb_device
+                              where tb_device_type.device_type_id=tb_device.device_type_id
+                              limit %d OFFSET %d''' %(pageSize, (currentPage-1)*pageSize))
             data = cursor.fetchall()
         except Exception as error:
             print(error.with_traceback())
--- api.py
+++ api.py
@@ -21,21 +21,33 @@ def myfun2():
     return 'myfun2'
 
 
-@api.route('MySQL', methods=['get', 'post'])
+@api.route('/MySQL', methods=['get', 'post'])
 def MySQL():
     mySQLService = MySQLService()
 
     return jsonify(mySQLService.getData())
 
 
-@api.route('PostgreSQL', methods=['get', 'post'])
+@api.route('/PostgreSQL', methods=['get', 'post'])
 def PostgreSQL():
     postgreSQLService = PostgreSQLService()
 
     return jsonify(postgreSQLService.getData())
 
 
-@api.route('tree', methods=['get', 'post'])
+@api.route('/tree', methods=['get', 'post'])
 def mytree():
     tr = tree()
     return jsonify(tr.get_root_department())
+
+
+@api.route('/get_device', methods=['get', 'post'])
+def get_device():
+    """分页取得设备列表"""
+    pageSize = request.values.get('pageSize')
+    currentPage = request.values.get('currentPage')
+
+    if pageSize is None or currentPage is None: return {'error': 'no pageSize currentPage'}
+
+    postgreSQLService = PostgreSQLService()
+    return jsonify(postgreSQLService.get_device_by_page(int(pageSize), int(currentPage)))
